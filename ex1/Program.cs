fcd6e7fd4ce9a2767b9533efb4ea7de7929fb6b6
Maxim Pechalov <magaterkolg@gmail.com>
2023-06-20 20:41:19 +0300

    hmdone

---
+++ ex1/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+
+Console.WriteLine("Enter first number: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter second number: ");
+int b = int.Parse(Console.ReadLine());
+
+int c = result(a,b);
+Console.WriteLine(c);
+
+int result (int firstNumber, int secondNumber)
+{
+    int i = 1;
+    int j = 1;
+    while (i <= secondNumber)
+    {
+        j *= firstNumber;
+        i++;
+    }
+    return j;
+}
---
+++ ex2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+
+Console.WriteLine("Enter your number: ");
+int num = int.Parse(Console.ReadLine());
+int result = Math.Abs(sum(num));
+Console.WriteLine($"Result = {result}");
+
+int sum (int number)
+{
+    int i = 0;
+    while (number != 0)
+    {
+        i += number % 10;
+        number /= 10;
+    }
+    return i;
+
+}
